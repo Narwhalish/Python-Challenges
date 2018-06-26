2c433222f95f26bc72e39adba08f5afa73f8ba66
Narwhalish <bryao@ctemc.org>
2018-06-26 17:00:08 -0400

    Changes to lxml-python-webscrape

---
+++ Medium/lxml_python_webscrape/scrape.py
@@ -0,0 +1,7 @@
+import requests
+import lxml.html
+
+html = requests.get('https://store.steampowered.com/explore/new/')
+doc = lxml.html.fromstring(html.content)
+
+new_releases = doc.xpath('//div[@id="tab_newreleases_content"]')[0]
