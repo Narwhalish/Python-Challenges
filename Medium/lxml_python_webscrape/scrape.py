b88313c7536c72d74219190ecca31f4104f41552
Narwhalish <bryao@ctemc.org>
2018-06-26 20:15:49 -0400

    Fix directories

--- Medium/lxml_python_webscrape/scrape.py
+++ Medium/lxml_python_webscrape/scrape.py
@@ -5,3 +5,7 @@ html = requests.get('https://store.steampowered.com/explore/new/')
 doc = lxml.html.fromstring(html.content)
 
 new_releases = doc.xpath('//div[@id="tab_newreleases_content"]')[0]
+
+titles = new_releases.xpath('.//div[@class="tab_item_name"]/text()')
+
+print(titles)
