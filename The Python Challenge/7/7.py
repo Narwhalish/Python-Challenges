7e460e15dd105b72d7590df9ae7b6a510a0b3cbb
Narwhalish <bryao@ctemc.org>
2018-07-15 22:37:42 -0400

    Complete Challenge 7

--- The Python Challenge/7/7.py
+++ The Python Challenge/7/7.py
@@ -2,8 +2,26 @@
 # -*- encoding: utf-8 -*-
 
 import requests
-from Pillow import PIL
+import re
+from PIL import Image
 from io import BytesIO
 
 r = requests.get('http://www.pythonchallenge.com/pc/def/oxygen.png')
 i = Image.open(BytesIO(r.content))
+
+bbox = i.getbbox()
+
+values = []
+colors = [chr(i.getpixel((m, bbox[3] / 2))[0]) for m in range(0, 629, 7)]
+
+for c in colors:
+    values.append(c)
+
+message = ''.join(values)
+
+pattern = re.compile('\[((?:\d+,? ?)+)+\]')
+match = re.search(pattern, message)
+
+answer = ''.join([chr(int(c)) for c in match.group(1).split(', ')])
+
+print(answer)
